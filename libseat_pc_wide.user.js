31b0bef38fa1b174b8c096c33db1df2c3e7c04ba
flash122u <2488104497@qq.com>
2026-05-15 19:40:41 +0800

    Relax reservation row alignment

--- libseat_pc_wide.user.js
+++ libseat_pc_wide.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JLU LibSeat PC Wide Layout
 // @namespace    local.libseat.pcwide
-// @version      1.16.7
+// @version      1.16.8
 // @description  Improve libseat.jlu.edu.cn desktop layout, seat map scale, cover images, and time inputs.
 // @match        https://libseat.jlu.edu.cn/*
 // @run-at       document-start
@@ -17,7 +17,7 @@
   const SEAT_MAP_PADDING = 24;
   const FACILITY_DOM_STABLE_MS = 120;
   const FACILITY_REVEAL_FALLBACK_MS = 450;
-  const SCRIPT_VERSION = "1.16.7";
+  const SCRIPT_VERSION = "1.16.8";
   const DAY_OPEN_TIME = "08:00";
   const DAY_CLOSE_TIME = "22:00";
   const DEFAULT_MIN_RESERVATION_MINUTES = 30;
@@ -323,8 +323,8 @@
         display: grid;
         grid-template-columns:
           64px
-          minmax(96px, 0.42fr)
-          minmax(180px, 0.82fr)
+          minmax(128px, 0.85fr)
+          minmax(112px, 0.62fr)
           minmax(82px, 0.45fr)
           minmax(82px, 0.45fr)
           auto
@@ -339,14 +339,15 @@
         box-sizing: border-box;
       }
 
-      .libseat-reserve-query-row .libseat-time-field:first-of-type,
-      .libseat-reserve-auto-row .libseat-time-field:first-of-type {
-        grid-column: 2 / 4;
-      }
-
-      .libseat-reserve-query-row .libseat-reserve-spacer,
-      .libseat-reserve-auto-row .libseat-reserve-spacer {
-        display: none;
+      .libseat-reserve-manual-row {
+        grid-template-columns:
+          64px
+          minmax(84px, 0.42fr)
+          minmax(190px, 1.05fr)
+          minmax(82px, 0.45fr)
+          minmax(82px, 0.45fr)
+          auto
+          minmax(240px, 1.2fr);
       }
 
       .libseat-reserve-auto-row {
@@ -585,8 +586,6 @@
       }
 
       .libseat-slot-empty.libseat-manual-slot-empty {
-        padding: 0 6px;
-        font-size: 11px;
         white-space: nowrap;
       }
 
